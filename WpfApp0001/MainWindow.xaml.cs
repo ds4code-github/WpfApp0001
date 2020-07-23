e502e8fb3c8e2e0be3a6e7ca7b84acce14b72b83
Dee Ess <DS4Code@outlook.de>
2020-07-23 22:26:31 +0200

    Timer

--- WpfApp0001/MainWindow.xaml.cs
+++ WpfApp0001/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp0001
 {
@@ -20,9 +21,37 @@ namespace WpfApp0001
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DispatcherTimer _animationsTimer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            _animationsTimer.Interval = TimeSpan.FromMilliseconds(500);
+            _animationsTimer.Tick += PositioniereBall;
+        }
+
+        private void PositioniereBall(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            var x = Canvas.GetLeft(Ball);
+            Canvas.SetLeft(Ball, x + 5);
+        }
+
+        private void StartStopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_animationsTimer.IsEnabled)
+            {
+                _animationsTimer.Stop();
+            } 
+            else
+            {
+                _animationsTimer.Start();
+            }
+            
+            var mitteX = Spielplatz.ActualWidth / 2 - 15;
+            var mitteY = Spielplatz.ActualHeight / 2 - 15;
+
+            Canvas.SetLeft(Ball, mitteX);
+            Canvas.SetTop(Ball, mitteY);
         }
     }
 }
